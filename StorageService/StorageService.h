58ca291a2e0a9789eb0129145d31407b6921c663
DimonUN <nikonorov_photo@mail.ru>
2022-03-30 10:20:13 +0300

    Добавлены материалы для начала работы над ДЗ

---
+++ StorageService/StorageService.h
@@ -0,0 +1,17 @@
+//
+//  StorageService.h
+//  StorageService
+//
+//  Created by Дмитрий Никоноров on 26.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StorageService.
+FOUNDATION_EXPORT double StorageServiceVersionNumber;
+
+//! Project version string for StorageService.
+FOUNDATION_EXPORT const unsigned char StorageServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorageService/PublicHeader.h>
+
